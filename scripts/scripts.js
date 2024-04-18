2bbdd3c14666df79fa8406af342bf7dbd95c7ba6
Carlos Soria Labrandero <111304785+ProgramCocoDev@users.noreply.github.com>
2024-04-18 18:34:18 +0200

    COMMIT FOR UPDATE AND GOING HOME

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,43 @@ for (let i = 0; i < 1000; i++) {
     star.style.opacity = Math.random();
     document.getElementById("body-container").appendChild(star);
   }
+  function downloadCV() {
+      // Crear un enlace temporal
+      const link = document.createElement('a');
+      link.href = './cv/cv-english.pdf'; // Cambia la ruta al archivo que deseas descargar
+      link.target = '_blank';
+      link.download = 'cv-carlos-soria.pdf'; // Cambia el nombre del archivo descargado
+
+      // Simular un clic en el enlace
+      link.click();
+  }
+
+  function showContentCPP(index){
+    const container = document.getElementById('cpp-content');
+    container.innerHTML = '';
+    switch(index){
+      case 0:
+        container.innerHTML = `<div class="container-text">
+        <p>TEXTO POR DEFECTO 1</p>
+      </div>`;
+        break;
+      case 1:
+        container.innerHTML = `      <div class="container-text">
+        <p>TEXTO POR DEFECTO 2</p>
+      </div>`;
+        break;
+      case 2:
+        container.innerHTML = `     <div class="container-text">
+        <p>TEXTO POR DEFECTO 3</p>
+      </div>`;
+        break;
+      case 3:
+        container.innerHTML = `<h3>Proyecto 4</h3>
+        <p>Este es el proyecto 4 de C++</p>`;
+        break;
+      case 4:
+        container.innerHTML = `<h3>Proyecto 5</h3>
+        <p>Este es el proyecto 5 de C++</p>`;
+        break;
+    }
+  }
